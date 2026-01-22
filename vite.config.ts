e008be7866b8a6b9b28c2231889eae58b8743217
rexong <rexonghanwei200035@gmail.com>
2026-01-22 14:51:18 +0800

    add base to vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const plugins = [react(), tailwindcss()];
 
 export default defineConfig({
   plugins,
+  base: "/tutor-landing-page/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
